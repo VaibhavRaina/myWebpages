53839f6ef2e22515dfeabfe5763ef673ed61689f
VaibhavRaina <122040517+VaibhavRaina@users.noreply.github.com>
2023-12-10 17:02:13 +0530

    Delete schemas.js

--- schemas.js
+++
@@ -1,8 +0,0 @@
-const joi = require(`joi`);
-module.export = joi.object({
-    title: joi.string().required(),
-    price: joi.number().required().min(0),
-    image: joi.string().required(),
-    location: joi.string().required(),
-    description: joi.string().required(),
-}).required()
